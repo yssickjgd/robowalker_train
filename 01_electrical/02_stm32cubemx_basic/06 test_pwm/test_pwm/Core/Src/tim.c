cbc07076151f5e74af8f06b33d028c4336f70a1f
yssickjgd <yssickjgd@mail.ustc.edu.cn>
2023-08-18 19:26:22 +0800

    修订UART教程

--- 01_electrical/02_stm32cubemx_basic/06 test_pwm/test_pwm/Core/Src/tim.c
+++ 01_electrical/02_stm32cubemx_basic/06 test_pwm/test_pwm/Core/Src/tim.c
@@ -42,7 +42,7 @@ void MX_TIM3_Init(void)
 
   /* USER CODE END TIM3_Init 1 */
   htim3.Instance = TIM3;
-  htim3.Init.Prescaler = 99;
+  htim3.Init.Prescaler = 0;
   htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim3.Init.Period = 499;
   htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
