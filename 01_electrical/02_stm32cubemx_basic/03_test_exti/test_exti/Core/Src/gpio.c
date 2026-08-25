fd4577f81d6b8c57282263afa6573548677659f0
yssickjgd <yssickjgd@mail.ustc.edu.cn>
2026-08-25 23:21:54 +0800

    修改按键消抖的cube中断优先级

--- 01_electrical/02_stm32cubemx_basic/03_test_exti/test_exti/Core/Src/gpio.c
+++ 01_electrical/02_stm32cubemx_basic/03_test_exti/test_exti/Core/Src/gpio.c
@@ -66,7 +66,7 @@ void MX_GPIO_Init(void)
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /* EXTI interrupt init*/
-  HAL_NVIC_SetPriority(EXTI0_IRQn, 0, 0);
+  HAL_NVIC_SetPriority(EXTI0_IRQn, 15, 0);
   HAL_NVIC_EnableIRQ(EXTI0_IRQn);
 
 }
